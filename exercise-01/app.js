ed1da6633a8051571d6376cd1dd0926adddb6f91
Zolzaya <zolzaya245@gmail.com>
2021-03-31 06:28:36 -0500

    Zola commit.

--- exercise-01/app.js
+++ exercise-01/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
-const client = new MongoClient('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient('mongodb://localhost:27017', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 let db;
 
 const app = express();
@@ -21,23 +24,22 @@ app.use((req, res, next) => {
         req.db = db.collection('schools');
         next();
     }
-})
+});
 ////////////// DO NOT TOUCH //////////
 // Get all data - FOR TESTING PURPOSES ONLY
 app.get('/', (req, res) => {
-    req.db.find({}).toArray((err, data) => res.json(data))
-})
+    req.db.find({}).toArray((err, data) => res.json(data));
+});
 ////////////// DO NOT TOUCH //////////
 // delete all data - FOR TESTING PURPOSES ONLY
 app.delete('/clear', async (req, res) => {
-    await req.db.removeMany({})
-    req.db.find({}).toArray((err, data) => res.json(data))
-
-})
+    await req.db.removeMany({});
+    req.db.find({}).toArray((err, data) => res.json(data));
+});
 ////////////// DO NOT TOUCH //////////
 // fill with dummy data - FOR TESTING PURPOSES ONLY
 app.post('/fill', async (req, res) => {
-    await req.db.insertOne(req.body, (err, results) => res.json(results))
+    await req.db.insertOne(req.body, (err, results) => res.json(results));
 });
 
 //////////////////////////////////////
@@ -51,43 +53,117 @@ app.post('/fill', async (req, res) => {
 // Add teacher
 app.post('/schools/:school_id/teachers', async (req, res) => {
     // YOUR QUERY HERE
-    req.db.find({}).toArray((err, data) => res.json(data))
-})
+    await req.db.updateOne(
+        { _id: parseInt(req.params.school_id) },
+        { $addToSet: { teachers: req.body } }
+    );
+    req.db.find({}).toArray((err, data) => res.json(data));
+});
 // Update teacher by ID
 app.patch('/schools/:school_id/teachers/:teacher_id', async (req, res) => {
     // YOUR QUERY HERE
-    req.db.find({}).toArray((err, data) => res.json(data))
-})
+    await req.db.updateOne(
+        {
+            _id: parseInt(req.params.school_id),
+            teachers: {
+                $elemMatch: {
+                    _id: parseInt(req.params.teacher_id),
+                },
+            },
+        }, // query
+        { $set: { 'teachers.$.name': req.body.name } } // update operation
+    );
+    req.db.find({}).toArray((err, data) => res.json(data));
+});
 // Delete teacher by ID
 app.delete('/schools/:school_id/teachers/:teacher_id', async (req, res) => {
     // YOUR QUERY HERE
-    req.db.find({}).toArray((err, data) => res.json(data))
-})
+    await req.db.updateOne(
+        {
+            _id: parseInt(req.params.school_id),
+            teachers: {
+                $elemMatch: {
+                    _id: parseInt(req.params.teacher_id),
+                },
+            },
+        }, // query
+        { $unset: { 'teachers.$': 1 } }
+    );
+    req.db.find({}).toArray((err, data) => res.json(data));
+});
 
 // Add a new student to specific course
 app.post('/schools/:school_id/courses/:course_id', async (req, res) => {
     // YOUR QUERY HERE
-    req.db.find({}).toArray((err, data) => res.json(data))
-})
-
+    await req.db.updateMany(
+        {
+            // query
+            _id: parseInt(req.params.school_id),
+            courses: {
+                $elemMatch: {
+                    _id: parseInt(req.params.course_id),
+                },
+            },
+        },
+        {
+            // update rule
+            $addToSet: { 'courses.$.students': req.body },
+        }
+    );
+    req.db.find({}).toArray((err, data) => res.json(data));
+});
 
 // Update a student's name
-app.patch('/schools/:school_id/courses/:course_id/:student_id', async (req, res) => {
-    // YOUR QUERY HERE  
-    req.db.find({}).toArray((err, data) => res.json(data))
-})
+app.patch(
+    '/schools/:school_id/courses/:course_id/:student_id',
+    async (req, res) => {
+        // YOUR QUERY HERE
+        await req.db.updateMany(
+            { _id: parseInt(req.params.school_id) },
+            {
+                $set: {
+                    'courses.$[course].students.$[student].name': req.body.name,
+                },
+            },
+            {
+                arrayFilters: [
+                    { 'course._id': parseInt(req.params.course_id) },
+                    { 'student._id': parseInt(req.params.student_id) },
+                ],
+            }
+        );
+        req.db.find({}).toArray((err, data) => res.json(data));
+    }
+);
 
 // Delete a student
-app.delete('/schools/:school_id/courses/:course_id/:student_id', async (req, res) => {
-    // YOUR QUERY HERE
-    req.db.find({}).toArray((err, data) => res.json(data))
-})
+app.delete(
+    '/schools/:school_id/courses/:course_id/:student_id',
+    async (req, res) => {
+        // YOUR QUERY HERE
+        req.db.updateOne(
+            { _id: parseInt(req.params.school_id) },
+            {
+                $unset: {
+                    'courses.$[course].students.$[student]': 1,
+                },
+            },
+            {
+                arrayFilters: [
+                    { 'course._id': parseInt(req.params.course_id) },
+                    { 'student._id': parseInt(req.params.student_id) },
+                ],
+            }
+        );
+        req.db.find({}).toArray((err, data) => res.json(data));
+    }
+);
 
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.json({
         message: err.message,
-        error: {}
+        error: {},
     });
 });
 
